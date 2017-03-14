e9f0f11ffd84ba97a5e68a1dce29929ba3511b34
SASIKUMAR SUGUMAR <sasisugumar@gmail.com>
2017-03-14 08:33:35 -0500

    Controller Changes

---
+++ eureka-client/src/main/java/feign/client/web/GraphController.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package feign.client.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author euro
+ *
+ */
+@RestController
+public class GraphController {
+	
+	
+	@RequestMapping(value="/getGraphDetails",method=RequestMethod.GET)
+	public String getGraphDetails(){
+		return "";
+	}
+
+}
---
+++ feign-client/src/main/java/feign/client/web/FeignController.java
@@ -0,0 +1,5 @@
+package feign.client.web;
+
+public class FeignController {
+
+}
