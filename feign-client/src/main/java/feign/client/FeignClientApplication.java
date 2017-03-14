1305751f8948994db5aa54982f34b549cffb4ce1
SASIKUMAR SUGUMAR <sasisugumar@gmail.com>
2017-03-14 07:55:33 -0500

    Initial Upload Feign Client Example

---
+++ feign-client/src/main/java/feign/client/FeignClientApplication.java
@@ -0,0 +1,12 @@
+package feign.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignClientApplication.class, args);
+	}
+}
